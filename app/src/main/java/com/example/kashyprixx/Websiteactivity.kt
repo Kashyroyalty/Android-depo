bc428f706713a70e628aba3d2060851f8146c70c
eMobilis <fathiabdikadir09@gmail.com>
2023-04-14 12:24:16 +0300

    first commit

---
+++ app/src/main/java/com/example/kashyprixx/Websiteactivity.kt
@@ -0,0 +1,11 @@
+package com.example.kashyprixx
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Websiteactivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_websiteactivity)
+    }
+}
